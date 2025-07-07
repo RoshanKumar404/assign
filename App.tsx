71eea1b545c2421eafcbc55a5e0ee82e29f3de2a
Roshankumar404 <roshankuar951@gmail.com>
2025-07-07 09:24:31 +0530

    fixed header icons

--- App.tsx
+++ App.tsx
@@ -4,18 +4,6 @@ import { io } from 'socket.io-client';
 import ChatScreen from './Src/Screens/chatScreen';
 
 export default function App() {
-  useEffect(() => {
-    const socket = io('http://192.168.1.61:3000'); 
-    socket.on('connect', () => {
-      console.log('Connected to Socket.io server');
-    });
-
-    return () => {
-      socket.disconnect();
-      console.log('Socket disconnected');
-    };
-  }, []);
-
   return (
     <View style={{ flex: 1 }}>
       <ChatScreen />
--- Src/components/Header.tsx
+++ Src/components/Header.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Octicons from 'react-native-vector-icons/Octicons'
 import { chatScreenStyles } from '../Styles/chatScreenStyles';
 
 const Header = () => {
@@ -12,22 +14,25 @@ const Header = () => {
   return (
     <View style={chatScreenStyles.headerContainer}>
       <TouchableOpacity style={chatScreenStyles.headerButton}>
-         <Text style={chatScreenStyles.headerTitle}>←</Text>
+          <Icon name="arrow-back-outline" size={30} color="black" />
+          {/* //<ion-icon name="arrow-back-outline"></ion-icon> */}
       </TouchableOpacity>
 
       <Text style={chatScreenStyles.headerTitle}>Trip 1</Text>
 
       <TouchableOpacity style={chatScreenStyles.headerButton}>
-      <Text style={chatScreenStyles.headerIcon}>&#9998;</Text>
+      <Icon name="create-outline" size={30} color="black" />
       </TouchableOpacity>
 
       <TouchableOpacity style={chatScreenStyles.headerButton} onPress={toggler}>
-         <Text style={chatScreenStyles.headerIcon}>&#8942;</Text>
+         <Icon name='ellipsis-vertical-outline' size={26} color='black'/>
       </TouchableOpacity>
 
       {Modalvisible && (
         <View style={chatScreenStyles.moreOptionsDropdown}>
+
           <Text style={chatScreenStyles.dropdownItem}>Members</Text>
+
           <Text style={chatScreenStyles.dropdownItem}>Share Number</Text>
           <Text style={chatScreenStyles.dropdownItem}>Report</Text>
         </View>
